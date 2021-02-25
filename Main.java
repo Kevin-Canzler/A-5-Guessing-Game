937ca191d0cec17d096b4bab555adfec5787df45
Kevin-Canzler <70658937+Kevin-Canzler@users.noreply.github.com>
2021-02-25 05:35:09 -0800

    One line reduction that makes a disproportional change to the difficulty of reading it

--- Main.java
+++ Main.java
@@ -5,24 +5,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-	static Scanner input = new Scanner(System.in);
-	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		int[] gameStats = {0, 0, 0};
 		loop: while(true) {
-			int currentGame = guessingGame();
+			int currentGame = guessingGame(input);
 			gameStats = new int[] {++gameStats[0], gameStats[1] + currentGame, gameStats[2] == 0 || currentGame < gameStats[2] ? currentGame : gameStats[2]};
-			if (!checkForYes("Do you want to play again?")) break loop;
+			if (!checkForYes("Do you want to play again?", input)) break loop;
 		}
 		System.out.println("Overall results:\nTotal games = " + gameStats[0] + "\nTotal guesses = " + gameStats[1]);
 		System.out.println("Guesses/game = " + String.format("%.1f", (double) (gameStats[1]) / (double) (gameStats[0])) + "\nBest game = " + gameStats[2]);
 	}
 	
-	public static int guessingGame() {
+	public static int guessingGame(Scanner input) {
 		int[] gameStats = {0, -1}; int randomNumber = new Random() {/*magic*/}.nextInt(100);
 		System.out.println("I'm thinking of a number between 1 and 100...\n" + randomNumber);
 		loop: while(true) {
-			gameStats = new int[] {++gameStats[0], askQuestionInt("Your guess?")};
+			gameStats = new int[] {++gameStats[0], askQuestionInt("Your guess?", input)};
 			if (gameStats[1] == randomNumber) break loop;
 			System.out.println((gameStats[1] > randomNumber ? "It's lower." : "It's higher."));
 		}
@@ -30,18 +29,18 @@ public class Main {
 		return gameStats[0];
 	}
 	
-	public static int askQuestionInt(String question) {
+	public static int askQuestionInt(String question, Scanner input) {
 		System.out.println(question);
 		try {
 			return input.nextInt();
 		} catch(Exception e) {
 			input.next();
-			return askQuestionInt("Put an int in you f*ck");
+			return askQuestionInt("Put an int in you f*ck", input);
 		}
 	}
 	
-	public static boolean checkForYes(String question) {
+	public static boolean checkForYes(String question, Scanner input) {
 		System.out.println(question);
-		return input.next().toLowerCase().charAt(0);
+		return input.next().toLowerCase().charAt(0) == 'y' ? true : false;
 	}
 }
